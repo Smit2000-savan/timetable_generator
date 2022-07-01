438ae02b4b5ff6e4b5fab79bddfd95cdf8f69447
Smit2000-savan <76870477+Smit2000-savan@users.noreply.github.com>
2022-07-01 21:33:21 +0530

    Delete templates/coordinator/sriapp directory

--- templates/coordinator/sriapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Prof, Course, Batch, Available
-
-admin.site.register(Prof)
-admin.site.register(Course)
-admin.site.register(Batch)
-admin.site.register(Available)
